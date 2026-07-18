4f71cbf9eae15a62dec3425407c55fd4cf623f3d
JaswanthPeruri <perurijaswanth101@gmail.com>
2026-07-18 15:24:45 +0530

    Day-01

---
+++ 03_variables.js
@@ -0,0 +1,29 @@
+/*
+==========================================
+Variables
+==========================================
+
+A variable is a container used to store data.
+
+Syntax
+
+let variableName = value;
+*/
+
+let name = "Jaswanth";
+
+let age = 20;
+
+let college = "GMRIT";
+
+console.log(name);
+
+console.log(age);
+
+console.log(college);
+
+// Updating variable
+
+age = 21;
+
+console.log("Updated Age =", age);
---
+++ 06_data_types.js
@@ -0,0 +1,51 @@
+/*
+==========================================
+Primitive Data Types
+==========================================
+*/
+
+// Number
+
+let age = 20;
+
+let price = 99.99;
+
+// String
+
+let name = "Jaswanth";
+
+// Boolean
+
+let isStudent = true;
+
+// Undefined
+
+let data;
+
+// Null
+
+let value = null;
+
+// BigInt
+
+let bigNumber = 123456789012345678901234567890n;
+
+// Symbol
+
+let id = Symbol("id");
+
+console.log(typeof age);
+
+console.log(typeof price);
+
+console.log(typeof name);
+
+console.log(typeof isStudent);
+
+console.log(typeof data);
+
+console.log(typeof value);
+
+console.log(typeof bigNumber);
+
+console.log(typeof id);
